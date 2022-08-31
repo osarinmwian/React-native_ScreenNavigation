5206a6d9f66da2b6b2ec0dae44ae702b4236ab86
osarinmwian <noel.osarinmwian@gmail.com>
2022-08-31 17:24:31 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+function ScreenA({navigation}){
+
+  const onPressHandler =()=>{
+     navigation.navigate("screenB")
+  }
+  return(
+ <View style={styles.container}>
+   <Text style={styles.text}>
+     screen A
+   </Text>
+   <Pressable
+   onPress={onPressHandler}
+   style={({pressed}) =>({backgroundColor: pressed? "#dd" : "#0f0" })}>
+      <Text style={styles.text}>
+     Go to screen B
+   </Text>
+     
+   </Pressable>
+   
+ </View>
+  )
+}
+function ScreenB({navigation}){
+  const onPressHandler =()=>{
+    navigation.navigate("screenA")
+    // navigation.goBack();
+
+ }
+  return(
+ <View style={styles.container}>
+   <Text style={styles.text}>
+     screen B
+   </Text>
+   <Pressable
+   onPress={onPressHandler}
+   style={({pressed}) =>({backgroundColor: pressed? "#dd" : "#0f0" })}>
+      <Text style={styles.text}>
+     Go to screen A
+   </Text>
+     
+   </Pressable>
+ </View>
+  )
+}
+ function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+     <Stack.Navigator
+     //to hide header in all screens
+    // screenOptions={{
+    //   header:()=> null
+    // }}
+      >
+       <Stack.Screen
+       name="screenA"
+       component={ScreenA}
+       options={{
+          //to hide header in a particular screen
+        header:()=> null
+      }}/>
+        <Stack.Screen
+       name="screenB"
+       component={ScreenB}
+      //  options={{
+      //    header:()=> null
+      //  }}
+      />
+        
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
@@ -17,4 +84,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:40,
+    fontWeight:"bold",
+    margin:10
+  }
 });
+export default App;
